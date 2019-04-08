ffaffd3dc654684e01e9e0b6ac9b05a43e9f2aa9
joel kohn <joel.kohn42@gmail.com>
2019-04-08 18:00:35 -0400

    protected route working

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 // Load User model
 const User = require("../../models/User");
 
@@ -90,5 +91,14 @@ router.post("/login", (req, res) => {
       return res.status(500).json({ message: "Internal server error" });
     });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => res.json({ msg: "success" })
+);
 
 module.exports = router;

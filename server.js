fede4738208994bdc9d14451ecc176534990ba75
joel kohn <joel.kohn42@gmail.com>
2019-04-09 15:39:25 -0400

    passport up

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
 app.use(passport.initialize());
 
 // Passport Config
-require("./config/passport")(passport);
+require(".wi/config/passport")(passport);
 
 // app.get("/", (req, res) => res.send("Hello Moombas!"));
 

8b305b7100f755aacf2d8af6b3bf7f984485f3f0
jo-codes <joel.kohn42@gmail.com>
2019-05-26 15:26:51 -0400

    logout and navbar links

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,10 +6,9 @@ const Landing = () => {
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Connector</h1>
+          <h1 className='x-large'>Moomba</h1>
           <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            A Social Network for the Wordy.
           </p>
           <div className='buttons'>
             <Link to='/register' className='btn btn-primary'>

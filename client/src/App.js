0bd0d3527a897da73ad625cde87852b89548c35e
Joel Kohn <joel.kohn42@gmail.com>
2019-07-02 20:23:32 -0400

    started dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/layout/Dashboard';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';

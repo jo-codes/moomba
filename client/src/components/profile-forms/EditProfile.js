710af2d703655e87f84799f062dfdd2513f5b0b6
Joel Kohn <joel.kohn42@gmail.com>
2019-09-08 19:57:10 +0300

    fixed some compilation warnings

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -44,7 +44,7 @@ const EditProfile = ({
         instagram: loading || !profile.instagram ? '' : profile.instagram
       });
     },
-    [loading],
+    [loading, getCurrentProfile],
     getCurrentProfile
   );
 
